8b78743f8ee57cd5acb3cdd2b0fa6c8d60fa55d6
Abdurahman Arafa Balah <59946744+Abdurahman-Arafa@users.noreply.github.com>
2022-12-21 02:59:48 +0200

    added SVM UI and some Algorithms

--- algorithms.py
+++ algorithms.py
@@ -1,10 +1,120 @@
-dataset=None;
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score , precision_score , recall_score
+from sklearn.feature_selection import RFE
 
-def SVM():
-  print("SVM ...")
+dataset=None
 
-def dession_tree():
-  print("dession tree ...")
+num_feature=None
 
-def KNN():
-  print("alg3 ...")
+
+#method to be called in each algorithm to get the reduced X and y.
+#the other way to do this is putting SVR("linear") as the estimator in the method and call the rfe method in the select_algorithm method.
+def rfe(X, y, estimator ,num_feature):
+  # Feature Extraction with RFE
+  selector = RFE(estimator = estimator, n_features_to_select= num_feature)
+  X = selector.fit_transform(X, y)
+  return X, y
+
+
+    
+  
+
+def SVM(kernel, test_size):
+  X = dataset.iloc[:, :-1].values
+  y = dataset.iloc[:, -1].values
+
+  #estimator = SVR(kernel="linear") linear becuase rbf does't have coef_ nor feature_importances_
+  X, y = rfe(X,y, SVC(kernel = "linear"), num_feature)
+
+  X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=0)
+
+  sc = StandardScaler()
+  X_train = sc.fit_transform(X_train)
+  X_test = sc.transform(X_test)
+
+  clf = SVC(kernel = kernel)
+  clf = clf.fit(X_train, y_train)
+
+  y_pred = clf.predict(X_test)
+
+  global conf_matrix, accuracy, precision, recall, f1_score
+
+  conf_matrix = confusion_matrix(y_test, y_pred)
+  accuracy= accuracy_score(y_test, y_pred)
+  precision = precision_score(y_test, y_pred)
+  recall = recall_score(y_test, y_pred)
+  f1_score = 2 * (precision * recall) / (precision + recall)
+
+
+
+
+def dession_tree(max_depth ,test_size):
+
+  #I generated this code using copilot, haven't tested it yet
+  #الكود بتاع الجزء ده تامر باعته نبقي نستخدمه هنا
+
+  X = dataset.iloc[:, :-1].values
+  y = dataset.iloc[:, -1].values
+
+  #need to test if the estimator has coef_ or feature_importances_
+  X, y = rfe(X,y, DecisionTreeClassifier(max_depth=max_depth), num_feature)
+  X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=0)
+
+  sc = StandardScaler()
+  X_train = sc.fit_transform(X_train)
+  X_test = sc.transform(X_test)
+
+  clf = DecisionTreeClassifier(max_depth=max_depth)
+  clf = clf.fit(X_train, y_train)
+
+  y_pred = clf.predict(X_test)
+
+  global conf_matrix, accuracy, precision, recall, f1_score
+
+  conf_matrix = confusion_matrix(y_test, y_pred)
+  accuracy= accuracy_score(y_test, y_pred)
+  precision = precision_score(y_test, y_pred)
+  recall = recall_score(y_test, y_pred)
+  f1_score = 2 * (precision * recall) / (precision + recall)
+  
+
+
+
+
+def KNN(test_size):
+  
+  #I generated this code using copilot, haven't tested it yet
+  #الكود للجزء ده مش مبعوت فممكن نستخدم ده وخلاص
+  #كل ال الباراميترز ليهم ديفولت فمش عارف نباصي انهي
+
+  X = dataset.iloc[:, :-1].values
+  y = dataset.iloc[:, -1].values
+
+
+  #need to test if the estimator has coef_ or feature_importances_
+  X, y = rfe(X,y, KNeighborsClassifier(), num_feature)
+  X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=0)
+
+  sc = StandardScaler()
+  X_train = sc.fit_transform(X_train)
+  X_test = sc.transform(X_test)
+
+  clf = KNeighborsClassifier()
+  clf = clf.fit(X_train, y_train)
+
+  y_pred = clf.predict(X_test)
+
+  global conf_matrix, accuracy, precision, recall, f1_score
+
+  conf_matrix = confusion_matrix(y_test, y_pred)
+  accuracy= accuracy_score(y_test, y_pred)
+  precision = precision_score(y_test, y_pred)
+  recall = recall_score(y_test, y_pred)
+  f1_score = 2 * (precision * recall) / (precision + recall)
--- main.py
+++ main.py
@@ -1,124 +1,215 @@
-import tkinter
-import customtkinter
-import algorithms
-
-customtkinter.set_appearance_mode("dark");
-customtkinter.set_default_color_theme("blue");
-
-app = customtkinter.CTk()
-app.geometry("400x500")
-
-inpt=None;
-
-
-# make a standard button
-def make_button(st,fun,pos):
-    
-  button = customtkinter.CTkButton(master=app,
-                                  text=st,
-                                  width=200,
-                                  command=fun);
-  button.place(relx=pos[0],rely=pos[1],anchor=tkinter.CENTER)
-
-# make a back button
-def make_back_button(fun):
-  button = customtkinter.CTkButton(master=app,
-                                  text="Back",
-                                  width=80,
-                                  command=fun);
-  button.place(relx=.25,rely=.85,anchor=tkinter.CENTER)
+import tkinter 
+from tkinter.filedialog import askopenfilenames 
+import customtkinter 
+import algorithms 
+import pandas as pd 
+ 
+customtkinter.set_appearance_mode("dark"); 
+customtkinter.set_default_color_theme("blue"); 
+ 
+app = customtkinter.CTk() 
+app.geometry("400x500") 
 
 
-# make astandard frame
-def make_frame():
-  frame = customtkinter.CTkFrame(master=app,
-                                width=300,
-                                height=400,
-                                corner_radius=10)
-  frame.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
+ 
 
-# make a standerd label
-def make_label(st,pos):
-  label = customtkinter.CTkLabel(master=app,
-                                text=st,
-                                width=200,
-                                height=25,
-                                  bg_color="#ffdf76",
-                                  text_color="#664e04",
-                                  font=("Arial", 18)
-                                )
-  label.place(relx=pos[0], rely=pos[1], anchor=tkinter.CENTER)
-
-
-def make_input(pos):
-  entry = customtkinter.CTkEntry(master=app,
-                                width=200,
-                                height=25,
-                                corner_radius=10)
-  entry.place(relx=pos[0], rely=pos[1], anchor=tkinter.CENTER)
-
-#----------------------------------------------------------------
-
-
+def is_float(value):
+  """Check if value is a float"""
+  try:
+      float(value)
+      return True
+  except ValueError:
+      return False
+ 
+# make a standard button 
+def make_button(st,fun,pos): 
+  """Make a standard button"""
+  button = customtkinter.CTkButton(master=app, 
+                                  text=st, 
+                                  width=200, 
+                                  command=fun); 
+  button.place(relx=pos[0],rely=pos[1],anchor=tkinter.CENTER) 
+ 
+# make a back button 
+def make_back_button(fun): 
+  button = customtkinter.CTkButton(master=app, 
+                                  text="Back", 
+                                  width=80, 
+                                  command=fun); 
+  button.place(relx=.25,rely=.85,anchor=tkinter.CENTER) 
+ 
+ 
+# make astandard frame 
+def make_frame(): 
+  frame = customtkinter.CTkFrame(master=app, 
+                                width=300, 
+                                height=400, 
+                                corner_radius=10) 
+  frame.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER) 
+ 
+# make a standerd label 
+def make_label(st,pos): 
+  label = customtkinter.CTkLabel(master=app, 
+                                text=st, 
+                                width=200, 
+                                height=25, 
+                                bg_color="#fff0e1", 
+                                text_color="#664e04", 
+                                font=("Arial", 18) 
+                                ) 
+  label.place(relx=pos[0], rely=pos[1], anchor=tkinter.CENTER) 
+ 
+ 
+ 
+def make_input(pos): 
+  entry = customtkinter.CTkEntry(master=app, 
+                                width=200, 
+                                height=25, 
+                                corner_radius=10) 
+  entry.place(relx=pos[0], rely=pos[1], anchor=tkinter.CENTER) 
+ 
+def make_option_menu(values, default, callback, pos): 
+    global choice 
+    combobox = customtkinter.CTkOptionMenu(master=app, 
+                                           values=values, 
+                                           command=callback) 
+    combobox.place(relx=pos[0],rely=pos[1],anchor=tkinter.CENTER) 
+    combobox.set(default) 
+#---------------------------------------------------------------- 
+ 
+ 
 def destroy_all():
-  for w in app.winfo_children():
-    w.destroy();
-
-
+  """Destroy all widgets""" 
+  for w in app.winfo_children(): 
+    w.destroy(); 
+ 
+ 
 def load_dataset():
-  print("load dataset ...")
-  # algorithms.dataset=
-
-
-#display algoritms windows
-
-def select_algorithm():
-  if algorithms.dataset is None:
-    print("Upload dataset first !");
-    #return 
-  if False : #val is None
-    print("enter feature number")
-  
-  if False : #val.is_integer()
-    print("Invalid numbers !");
-    #return
-
-    
-  destroy_all();
-  make_frame();
-  make_label("Select Algorithm", [.5,.3]);
-  make_button("SVM",algorithms.SVM, [.5,.4]);
-  make_button("Dession Tree",algorithms.dession_tree, [.5,.5]);
-  make_button("KNN",algorithms.KNN, [.5,.6]);
-  make_back_button(classification);
-  
-
-
-#classification window
-
-def classification():
-
-  destroy_all();
-  make_frame();
-  make_button("Load Dataset", load_dataset, [.5,.3]);
-  make_label("Enter feature number", [.5,.4])
-  make_input([.5,.5]);
-  make_button("Submmit", select_algorithm, [.5,.7]);
-  make_back_button(main_window);
-
-
-
-
-
-def main_window():
-  
-  make_frame();
-  make_label("machine learning project", [.5,.3])
-  make_button("Classification", classification, [.5,.5]);
-
-
+  """prompt user to select a file and load it into dataset""" 
+  csv_file_path = askopenfilenames() 
+  algorithms.dataset=pd.read_csv(csv_file_path[0]) 
    
-main_window();
+    
+   
+ 
+ 
+#display algoritms windows 
+ #n_features = number of features for RFE
+def select_algorithm(n_features): 
+  """Select algorithm window"""
+  if algorithms.dataset is None: 
+   tkinter.messagebox.showerror("Dataset Error","UPLOAD DATASET, PLEASE!!") 
+   return
+ 
+  if n_features == "": 
+    tkinter.messagebox.showerror("Features Error ","ENTER NUMBER OF FEATURES, PLEASE!! ") 
+    return
+   
+  if  n_features.isdigit() == False: 
+    tkinter.messagebox.showerror("Features Error ","INVALID NUMBER !! ") 
+    return
+  
+  algorithms.num_feature = int(n_features)
+  # algorithms.RFE()
+  destroy_all(); 
+  make_frame(); 
+  make_label("Select Algorithm", [.5,.3]); 
+  make_button("SVM",SVM_window, [.5,.4]); 
+  make_button("Dession Tree",algorithms.dession_tree, [.5,.5]); 
+  make_button("KNN",algorithms.KNN, [.5,.6]); 
+  make_back_button(classification); 
+   
+ 
+ 
+#classification window 
+ 
+def classification(): 
+  """Classification window"""
+  destroy_all(); 
+  make_frame(); 
+  make_button("Load Dataset", load_dataset, [.5,.3]); 
+  make_label("Enter feature number", [.5,.4]) 
+ 
+  entry = customtkinter.CTkEntry(master=app, 
+                                 width=200, 
+                                height=25, 
+                                  corner_radius=10) 
+  entry.place(relx=.5, rely=.5, anchor=tkinter.CENTER) 
+ 
+  def get_input(): 
+    #the entry.get() here is the number of features
+    select_algorithm(entry.get()); 
+ 
+  make_button("Submmit", get_input, [.5,.7]); 
+ 
+  make_back_button(main_window); 
+ 
+ 
+ 
+ 
+ 
+ 
+def main_window(): 
+  destroy_all();
+  make_frame() 
+  make_label("machine learning project", [.5,.3]) 
+  make_button("Classification", classification, [.5,.5]) 
+ 
+ 
+def SVM_window():
+  destroy_all(); 
+  make_frame(); 
+  make_label("SVM", [.5,.3]) 
+  #function that assigns the selected choice to the global variable kernel 
+  def kernelmenu_callback(choice): 
+    global kernel
+    if choice == "":
+      kernel = "rbf"
+    else:
+      kernel = choice 
+  make_option_menu(["linear", "rbf"], "rbf", kernelmenu_callback, [.5,.4]) 
+  make_button("Submmit", train_test, [.5,.7]) 
+  make_back_button(select_algorithm) 
+ 
+def train_test(): 
+  destroy_all(); 
+  make_frame(); 
+  make_label("Train & Test", [.5,.3]) 
+  make_label("Enter test size", [.5,.4]) 
+  entry = customtkinter.CTkEntry(master=app, 
+                                 width=200, 
+                                 height=25, 
+                                 corner_radius=10) 
+  entry.place(relx=.5, rely=.5, anchor=tkinter.CENTER) 
 
 
+  def get_input(): 
+    if is_float(entry.get()) == False:
+      tkinter.messagebox.showerror("Test size Error","INVALID TEST SIZE !! ")
+      return
+    algorithms.SVM(kernel,float(entry.get()));  #the entry.get() is the test size
+    show_result()
+
+  make_button("Submmit", get_input, [.5,.7]) 
+  make_back_button(SVM_window)
+ 
+
+def show_result():
+  destroy_all(); 
+  make_frame(); 
+  make_label("Results", [.5,.18]) 
+  make_label("Accuracy", [.5,.27]) 
+  make_label(str(algorithms.accuracy), [.5,.32]) 
+  make_label("Precision", [.5,.42]) 
+  make_label(str(algorithms.precision), [.5,.47]) 
+  make_label("Recall", [.5,.57]) 
+  make_label(algorithms.recall, [.5,.62]) 
+  make_label("F1 Score", [.5,.72])
+  make_label(algorithms.f1_score, [.5,.77])
+  make_back_button(main_window)
+    
+main_window(); 
+
+
+ 
 app.mainloop()
